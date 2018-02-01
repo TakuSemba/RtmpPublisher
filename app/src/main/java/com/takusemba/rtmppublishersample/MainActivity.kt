d26d75dbe9f7eb148e22a832f8692742f148dd7c
takusemba <taku1008ele@gmail.com>
2018-02-01 23:24:10 +0900

    code format for smaple

--- app/src/main/java/com/takusemba/rtmppublishersample/MainActivity.kt
+++ app/src/main/java/com/takusemba/rtmppublishersample/MainActivity.kt
@@ -10,38 +10,38 @@ import com.takusemba.rtmppublisher.RtmpPublisher
 
 class MainActivity : AppCompatActivity() {
 
-  private val publisher = RtmpPublisher()
+    private val publisher = RtmpPublisher()
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    val glView: GLSurfaceView = findViewById(R.id.surface_view)
-    publisher.initialize(this, glView)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val glView: GLSurfaceView = findViewById(R.id.surface_view)
+        publisher.initialize(this, glView)
 
-    findViewById<Button>(R.id.toggle_publish).setOnClickListener {
-      if (publisher.isPublishing) {
-        publisher.stopPublishing()
-      } else {
-        publisher.startPublishing(BuildConfig.STREAMING_URL)
-      }
-      updateControls()
+        findViewById<Button>(R.id.toggle_publish).setOnClickListener {
+            if (publisher.isPublishing) {
+                publisher.stopPublishing()
+            } else {
+                publisher.startPublishing(BuildConfig.STREAMING_URL)
+            }
+            updateControls()
+        }
+
+        findViewById<ImageView>(R.id.toggle_camera).setOnClickListener {
+            publisher.switchCamera()
+        }
     }
 
-    findViewById<ImageView>(R.id.toggle_camera).setOnClickListener {
-      publisher.switchCamera()
+    override fun onResume() {
+        super.onResume()
+        updateControls()
     }
-  }
 
-  override fun onResume() {
-    super.onResume()
-    updateControls()
-  }
-
-  private fun updateControls() {
-    val publishButton: Button = findViewById(R.id.toggle_publish)
-    publishButton.text = getString(
-        if (publisher.isPublishing) R.string.stop_publishing else R.string.start_publishing)
-    publishButton.background = ContextCompat.getDrawable(this,
-        if (publisher.isPublishing) R.drawable.round_corner_white_dark else R.drawable.round_corner_white_dark)
-  }
+    private fun updateControls() {
+        val publishButton: Button = findViewById(R.id.toggle_publish)
+        publishButton.text = getString(if (publisher.isPublishing)
+            R.string.stop_publishing else R.string.start_publishing)
+        publishButton.background = ContextCompat.getDrawable(this, if (publisher.isPublishing)
+            R.drawable.round_corner_white_dark else R.drawable.round_corner_white_dark)
+    }
 }
